8ff7bb97eb12c1595b3ac9cd85f0d2e6a9597841
Natalya Anatskaya <taff2011@yandex.ru>
2021-11-21 20:47:46 +0500

    Diploma_frontend_final_for)server

--- src/utils/auth.js
+++ src/utils/auth.js
@@ -1,4 +1,4 @@
-export const BASE_URL = 'http://api.movies.diploma.nomoredomains.monster'
+export const BASE_URL = 'https://api.movies.diploma.nomoredomains.monster'
 //export const BASE_URL = 'http://localhost:3000'
 //export const BASE_URL = 'http://localhost:3001'
 
