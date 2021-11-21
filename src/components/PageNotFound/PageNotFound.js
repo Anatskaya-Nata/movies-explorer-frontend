2c34456176e8244ab5eebb36e4be0aa756ce267d
Natalya Anatskaya <taff2011@yandex.ru>
2021-11-22 01:57:43 +0500

    Diploma_frontend_final_for)server

--- src/components/PageNotFound/PageNotFound.js
+++ src/components/PageNotFound/PageNotFound.js
@@ -6,7 +6,7 @@ function PageNotFound(props) {
 	return (
 		<div className='error'>
 			<h2 className='error__title'>404</h2>
-			<h2 className='error__subtitle'>Страница не найдена</h2>
+			<h2 className='error__subtitle'> Страница не найдена </h2>
 
 			<Link to='/' className='error__back'>
 				Назад
